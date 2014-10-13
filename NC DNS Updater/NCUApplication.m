bb2ca35af851fc34f7043870a3197f1a8f1c306b
Spencer Diniz <yc26sd1@macys.com>
2014-10-13 14:12:44 -0300

    Enabled copy/paste.

---
+++ NC DNS Updater/NCUApplication.m
@@ -0,0 +1,47 @@
+//
+//  NCUApplication.m
+//  NC DNS Updater
+//
+//  Created by Spencer Müller Diniz on 13/10/14.
+//  Copyright (c) 2014 SPENCER. All rights reserved.
+//
+
+#import "NCUApplication.h"
+
+@implementation NCUApplication
+
+- (void) sendEvent:(NSEvent *)event {
+    if ([event type] == NSKeyDown) {
+        if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == NSCommandKeyMask) {
+            if ([[event charactersIgnoringModifiers] isEqualToString:@"x"]) {
+                if ([self sendAction:@selector(cut:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"c"]) {
+                if ([self sendAction:@selector(copy:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"v"]) {
+                if ([self sendAction:@selector(paste:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"z"]) {
+                if ([self sendAction:@selector(undo:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"a"]) {
+                if ([self sendAction:@selector(selectAll:) to:nil from:self])
+                    return;
+            }
+        }
+        else if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == (NSCommandKeyMask | NSShiftKeyMask)) {
+            if ([[event charactersIgnoringModifiers] isEqualToString:@"Z"]) {
+                if ([self sendAction:@selector(redo:) to:nil from:self])
+                    return;
+            }
+        }
+    }
+    [super sendEvent:event];
+}
+
+@end
