0052e01817536451123ca731229f1c9ff7f66205
Spencer Diniz <yc26sd1@macys.com>
2015-01-21 14:55:25 -0200

    Created NC DNS Updater Daemon project.

---
+++ NC DNS Updater Daemon/NC DNS Updater Daemon/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NC DNS Updater Daemon
+//
+//  Created by Spencer Müller Diniz on 21/01/15.
+//  Copyright (c) 2015 LARATECH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
